1e058f4fefea62aae7d54f81532c951d86af61ab
Steven Inouye <steveninouye@msn.com>
2018-11-02 16:42:13 -0700

    Complete implementations of hash maps

--- hash_map_and_lru_cache/lib/p05_hash_map.rb
+++ hash_map_and_lru_cache/lib/p05_hash_map.rb
@@ -1,6 +1,8 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
+
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -9,18 +11,36 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    if include?(key)
+      bucket(key).update(key, val)
+    else
+      resize! if @count == num_buckets
+      bucket(key).append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    if include?(key)
+      bucket(key).remove(key)
+      @count -= 1
+    end
   end
 
-  def each
+  def each(&prc)
+    @store.each do |list|
+      list.each do |node|
+        prc.call(node.key,node.val)
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
@@ -41,9 +61,18 @@ class HashMap
   end
 
   def resize!
+    num_buck = num_buckets
+    arr = @store
+    @store = Array.new(num_buck * 2) { LinkedList.new }
+    @count = 0
+    arr.each do |list|
+      list.each do |node|
+        self.set(node.key,node.val)
+      end
+    end
   end
 
   def bucket(key)
-    # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % num_buckets]
   end
 end
