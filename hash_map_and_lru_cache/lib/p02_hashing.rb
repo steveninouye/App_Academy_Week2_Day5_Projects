dc9260606c025dcd20d62d2835c2705548fe5c17
Steven Inouye <steveninouye@msn.com>
2018-11-06 09:18:39 -0800

    Add completed Hashing file

--- hash_map_and_lru_cache/lib/p02_hashing.rb
+++ hash_map_and_lru_cache/lib/p02_hashing.rb
@@ -1,34 +1,24 @@
 class Fixnum
-  # Fixnum#hash already implemented for you
-<<<<<<< HEAD
-
-=======
->>>>>>> b6031a62a537f47e8f7c0139b8fa5e5280ba4662
+  # Fixnum#hash already implemented
 end
 
 class Array
   def hash
-<<<<<<< HEAD
     result = Integer(29.to_s(2))
     each_with_index do |el, i|
       result *= (el*i).hash
     end
     result
-=======
->>>>>>> b6031a62a537f47e8f7c0139b8fa5e5280ba4662
   end
 end
 
 class String
   def hash
-<<<<<<< HEAD
     result = Integer(29.to_s(2))
     chars.each_with_index do |ch, i|
       result *= (ch.ord*i).hash
     end
     result
-=======
->>>>>>> b6031a62a537f47e8f7c0139b8fa5e5280ba4662
   end
 end
 
@@ -36,17 +26,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-<<<<<<< HEAD
     result = Integer(29.to_s(2))
     each do |k, v|
       result *= (k.to_s.ord*v.ord).hash
     end
     result
   end
-
-
-=======
-    0
-  end
->>>>>>> b6031a62a537f47e8f7c0139b8fa5e5280ba4662
 end
