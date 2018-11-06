02b101540f71e68f840b269db2390c1efaa82734
Steven Inouye <steveninouye@msn.com>
2018-11-06 09:13:55 -0800

    Removed git extra syntax

--- hash_map_and_lru_cache/lib/p01_int_set.rb
+++ hash_map_and_lru_cache/lib/p01_int_set.rb
@@ -1,6 +1,5 @@
 class MaxIntSet
   def initialize(max)
-<<<<<<< HEAD
     @max = max
     @store = Array.new(max) {false}
   end
@@ -16,34 +15,14 @@ class MaxIntSet
 
   def include?(num)
     @store[num]
-=======
-  end
-
-  def insert(num)
-  end
-
-  def remove(num)
-  end
-
-  def include?(num)
->>>>>>> b6031a62a537f47e8f7c0139b8fa5e5280ba4662
   end
 
   private
 
   def is_valid?(num)
-<<<<<<< HEAD
     num.between?(0, @store.size-1)
   end
-  #
-  # def validate!(num)
-  # end
-=======
-  end
 
-  def validate!(num)
-  end
->>>>>>> b6031a62a537f47e8f7c0139b8fa5e5280ba4662
 end
 
 
@@ -53,7 +32,6 @@ class IntSet
   end
 
   def insert(num)
-<<<<<<< HEAD
     # idx = num % @store.length
     self[num] << num unless include?(num)
   end
@@ -64,24 +42,12 @@ class IntSet
 
   def include?(num)
     self[num].include?(num)
-=======
-  end
-
-  def remove(num)
-  end
-
-  def include?(num)
->>>>>>> b6031a62a537f47e8f7c0139b8fa5e5280ba4662
   end
 
   private
 
   def [](num)
-<<<<<<< HEAD
     @store[num % num_buckets]
-=======
->>>>>>> b6031a62a537f47e8f7c0139b8fa5e5280ba4662
-    # optional but useful; return the bucket corresponding to `num`
   end
 
   def num_buckets
@@ -98,7 +64,6 @@ class ResizingIntSet
   end
 
   def insert(num)
-<<<<<<< HEAD
     unless include?(num)
       resize! if @count == num_buckets
       @count += 1
@@ -115,24 +80,12 @@ class ResizingIntSet
 
   def include?(num)
     self[num].include?(num)
-=======
-  end
-
-  def remove(num)
-  end
-
-  def include?(num)
->>>>>>> b6031a62a537f47e8f7c0139b8fa5e5280ba4662
   end
 
   private
 
   def [](num)
-<<<<<<< HEAD
     @store[num % num_buckets]
-=======
->>>>>>> b6031a62a537f47e8f7c0139b8fa5e5280ba4662
-    # optional but useful; return the bucket corresponding to `num`
   end
 
   def num_buckets
@@ -140,17 +93,14 @@ class ResizingIntSet
   end
 
   def resize!
-<<<<<<< HEAD
-    num_buck = num_buckets
+    prev_num_buckets = num_buckets
     arr = @store
-    @store = Array.new(num_buck * 2) { Array.new }
+    @store = Array.new(prev_num_buckets * 2) { Array.new }
     @count = 0
     arr.each do |bucket|
       bucket.each do |el|
         self.insert(el)
       end
     end
-=======
->>>>>>> b6031a62a537f47e8f7c0139b8fa5e5280ba4662
   end
 end
